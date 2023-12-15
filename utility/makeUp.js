58e57b17de4c90d1feb14e76aa34903623928069
Hein Soe <35089574+hheinsoee@users.noreply.github.com>
2023-12-15 14:13:28 +0630

    Create makeUp.js
    to

---
+++ utility/makeUp.js
@@ -0,0 +1,47 @@
+export const makeUp = (data, pretty, isNull = null) => {
+  function mUp(d) {
+    const prettyData = {};
+    pretty.forEach((x) => {
+      // if (d[x] && d[x] !== "null") { prettyData[x] = d[x] }
+      // if (isNull && (d[x] == "undefined" || d[x] == null)) {
+      //   d[x] = null;
+      // }
+
+      if (d[x]) {
+        prettyData[x] = d[x];
+      } else if (isNull) {
+        prettyData[x] = null;
+      }
+    });
+    return prettyData;
+  }
+  if (Array.isArray(data)) {
+    const result = [];
+    data.forEach((d) => {
+      result.push(mUp(d));
+    });
+    return result;
+  }
+  return mUp(data);
+};
+
+export const isRequired = (data, need) => {
+  function mUp() {
+    const required = [];
+    need.forEach((x) => {
+      if (!data[x]) {
+        required.push(x);
+      }
+    });
+    return required.length === 0 ? false : required;
+  }
+  if (Array.isArray(data)) {
+    const result = [];
+    data.forEach((d) => {
+      result.push(...mUp(d));
+    });
+    return result.length === 0 ? false : result;
+  }
+
+  return mUp(data);
+};
